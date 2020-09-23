6ebcf5f213e5b3c5ae84657507e6e63208529719
simplepro <simplepro1254@gmail.com>
2020-09-23 18:12:10 +0900

    재부팅시 Room 안에 있는 데이터를 재설정하도록 구현함.

---
+++ app/src/main/java/com/wotin/geniustest/Receiver/RebootReceiver.kt
@@ -0,0 +1,18 @@
+package com.wotin.geniustest.Receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.wotin.geniustest.Service.RebootTestHeartManagementService
+
+class RebootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if ("android.intent.action.BOOT_COMPLETED" == intent!!.action) {
+            val service = Intent(context, RebootTestHeartManagementService::class.java)
+            ContextCompat.startForegroundService(context!!.applicationContext, service)
+        }
+    }
+}
