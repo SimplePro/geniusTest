9db4854643136dd636a1d5aebf39b32d2ee99ef8
simplepro <simplepro1254@gmail.com>
2020-09-25 16:51:44 +0900

    매니페스트에 특정권한을 더 추가하였고, RebootReceiver 안에 있는 if 문에서 조건을 하나더 추가하였다.

--- app/src/main/java/com/wotin/geniustest/Receiver/RebootReceiver.kt
+++ app/src/main/java/com/wotin/geniustest/Receiver/RebootReceiver.kt
@@ -10,7 +10,7 @@ import com.wotin.geniustest.Service.RebootTestHeartManagementService
 class RebootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if ("android.intent.action.BOOT_COMPLETED" == intent!!.action) {
+        if ("android.intent.action.BOOT_COMPLETED" == intent!!.action || Intent.ACTION_BOOT_COMPLETED == intent.action) {
             val service = Intent(context, RebootTestHeartManagementService::class.java)
             ContextCompat.startForegroundService(context!!.applicationContext, service)
         }
