6fcccff4b817e52d8c8c56f471672068346ff338
simplepro <simplepro1254@gmail.com>
2020-10-19 18:38:47 +0900

    RebootReceiver.kt 의 onReceive 일 때 로그인 상태인지 확인하고 Room 을 조정하도록 구현하였다.

--- app/src/main/java/com/wotin/geniustest/Receiver/RebootReceiver.kt
+++ app/src/main/java/com/wotin/geniustest/Receiver/RebootReceiver.kt
@@ -5,14 +5,39 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.content.ContextCompat
+import androidx.room.Room
+import com.wotin.geniustest.CustomClass.UserCustomClass
+import com.wotin.geniustest.DB.UserDB
+import com.wotin.geniustest.RoomMethod.UserRoomMethod
 import com.wotin.geniustest.Service.RebootTestHeartManagementService
 
 class RebootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if ("android.intent.action.BOOT_COMPLETED" == intent!!.action || Intent.ACTION_BOOT_COMPLETED == intent.action) {
-            val service = Intent(context, RebootTestHeartManagementService::class.java)
-            ContextCompat.startForegroundService(context!!.applicationContext, service)
+        try {
+            val userData = getUserData(context)
+            if (userData != null) {
+                if ("android.intent.action.BOOT_COMPLETED" == intent!!.action || Intent.ACTION_BOOT_COMPLETED == intent.action) {
+                    val service = Intent(context, RebootTestHeartManagementService::class.java)
+                    ContextCompat.startForegroundService(context!!.applicationContext, service)
+                }
+            } else {
+                Log.d("TAG", "RebootReceiver : onReceive userData is null")
+            }
+        } catch (e : Exception) {
+            Log.d("TAG", "RebootReceiver : onReceive userData is catch")
         }
+
+    }
+
+
+    private fun getUserData(context: Context?) : UserCustomClass {
+        val userDB : UserDB = Room.databaseBuilder(
+            context!!.applicationContext,
+            UserDB::class.java, "user.db"
+        ).allowMainThreadQueries()
+            .build()
+        val userData : UserCustomClass = userDB.userDB().getAll()
+        return userData
     }
 }
