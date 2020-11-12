a0bd87a69e3fc38a06e63496e4dcbf8a2d0745a7
simplepro <simplepro1254@gmail.com>
2020-11-12 18:04:35 +0900

    DeleteUserActivity 에 mvvm 패턴 적용 완료.

--- app/src/main/java/com/wotin/geniustest/activity/userManagement/DeleteUserActivity.kt
+++ app/src/main/java/com/wotin/geniustest/activity/userManagement/DeleteUserActivity.kt
@@ -8,19 +8,21 @@ import android.net.http.HttpResponseCache
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.room.Room
 import com.wotin.geniustest.activity.loginAndSignUp.LoginActivity
 import com.wotin.geniustest.activity.MainActivity
 import com.wotin.geniustest.database.UserDB
 import com.wotin.geniustest.EncryptionAndDetoxification
 import com.wotin.geniustest.R
+import com.wotin.geniustest.customClass.UserCustomClass
+import com.wotin.geniustest.databinding.ActivityDeleteUserBinding
 import com.wotin.geniustest.receiver.TestHeartManagementReceiver
 import com.wotin.geniustest.retrofitBuilder.GeniusRetrofitBuilder
 import com.wotin.geniustest.roomMethod.DeleteRoomMethod
 import com.wotin.geniustest.service.ConcentractionTestHeartManagementService
 import com.wotin.geniustest.service.MemoryTestHeartManagementService
 import com.wotin.geniustest.service.QuicknessTestHeartManagementService
-import kotlinx.android.synthetic.main.activity_delete_user.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,54 +30,58 @@ import java.lang.Exception
 
 class DeleteUserActivity : AppCompatActivity() {
 
+    lateinit var mBinding : ActivityDeleteUserBinding
+    lateinit var userData : UserCustomClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_delete_user)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_delete_user)
+        mBinding.activity = this
 
         val userDB : UserDB = Room.databaseBuilder(
             applicationContext,
             UserDB::class.java, "user.db"
         ).allowMainThreadQueries()
             .build()
-        val userData = userDB.userDB().getAll()
+        userData = userDB.userDB().getAll()
 
-        go_to_mainactivity_from_delete_account_imageview.setOnClickListener {
+        mBinding.goToMainactivityFromDeleteAccountImageview.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
 
-        delete_account_confirm_button.setOnClickListener {
-            if(check_password_before_delete_account_edittext.text.toString() == EncryptionAndDetoxification().encryptionAndDetoxification(userData.password)) {
-                GeniusRetrofitBuilder.deleteAccountApiService.deleteAccountAndData(userData.UniqueId).enqueue(object : Callback<HttpResponseCache> {
-                    override fun onFailure(call: Call<HttpResponseCache>, t: Throwable) {
+    fun clickedDeleteAccountButton() {
+        if(mBinding.checkPasswordBeforeDeleteAccountEdittext.text.toString() == EncryptionAndDetoxification().encryptionAndDetoxification(userData.password)) {
+            GeniusRetrofitBuilder.deleteAccountApiService.deleteAccountAndData(userData.UniqueId).enqueue(object : Callback<HttpResponseCache> {
+                override fun onFailure(call: Call<HttpResponseCache>, t: Throwable) {
+                    Toast.makeText(applicationContext, "계정이 삭제되지 않았습니다.", Toast.LENGTH_LONG).show()
+                }
+                override fun onResponse(
+                    call: Call<HttpResponseCache>,
+                    response: Response<HttpResponseCache>
+                ) {
+                    if(response.code() == 204) {
+                        Toast.makeText(applicationContext, "성공적으로 계정이 삭제되었습니다", Toast.LENGTH_LONG).show()
+                        DeleteRoomMethod().deleteUserDataAndGeniusTestAndPracticeData(applicationContext)
+                        Toast.makeText(applicationContext, "${userData.name} 님 그동안 '천재 테스트' 를 즐겨주셔서 감사합니다.", Toast.LENGTH_LONG).show()
+                        deleteUserDataSharedPreference()
+                        DeleteRoomMethod().deleteTestModeData(applicationContext)
+                        stopService(Intent(this@DeleteUserActivity, MemoryTestHeartManagementService::class.java))
+                        stopService(Intent(this@DeleteUserActivity, ConcentractionTestHeartManagementService::class.java))
+                        stopService(Intent(this@DeleteUserActivity, QuicknessTestHeartManagementService::class.java))
+                        cancelAlarm()
+                        val intent = Intent(this@DeleteUserActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
                         Toast.makeText(applicationContext, "계정이 삭제되지 않았습니다.", Toast.LENGTH_LONG).show()
                     }
-                    override fun onResponse(
-                        call: Call<HttpResponseCache>,
-                        response: Response<HttpResponseCache>
-                    ) {
-                        if(response.code() == 204) {
-                            Toast.makeText(applicationContext, "성공적으로 계정이 삭제되었습니다", Toast.LENGTH_LONG).show()
-                            DeleteRoomMethod().deleteUserDataAndGeniusTestAndPracticeData(applicationContext)
-                            Toast.makeText(applicationContext, "${userData.name} 님 그동안 '천재 테스트' 를 즐겨주셔서 감사합니다.", Toast.LENGTH_LONG).show()
-                            deleteUserDataSharedPreference()
-                            DeleteRoomMethod().deleteTestModeData(applicationContext)
-                            stopService(Intent(this@DeleteUserActivity, MemoryTestHeartManagementService::class.java))
-                            stopService(Intent(this@DeleteUserActivity, ConcentractionTestHeartManagementService::class.java))
-                            stopService(Intent(this@DeleteUserActivity, QuicknessTestHeartManagementService::class.java))
-                            cancelAlarm()
-                            val intent = Intent(this@DeleteUserActivity, LoginActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            Toast.makeText(applicationContext, "계정이 삭제되지 않았습니다.", Toast.LENGTH_LONG).show()
-                        }
-                    }
-                })
-            } else {
-                Toast.makeText(applicationContext, "비밀번호가 맞지 않습니다.", Toast.LENGTH_LONG).show()
-            }
+                }
+            })
+        } else {
+            Toast.makeText(applicationContext, "비밀번호가 맞지 않습니다.", Toast.LENGTH_LONG).show()
         }
     }
 
