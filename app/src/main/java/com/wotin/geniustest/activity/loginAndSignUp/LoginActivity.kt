935a159df92ec472de0da0fe7d5879bd0b1b68b5
simplepro <simplepro1254@gmail.com>
2020-11-12 16:27:08 +0900

    LoginActivity mvvm 패턴 적용 완료.

--- app/src/main/java/com/wotin/geniustest/activity/loginAndSignUp/LoginActivity.kt
+++ app/src/main/java/com/wotin/geniustest/activity/loginAndSignUp/LoginActivity.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.gson.JsonArray
 import com.wotin.geniustest.*
 import com.wotin.geniustest.activity.MainActivity
@@ -17,6 +18,7 @@ import com.wotin.geniustest.customClass.geniusPractice.GeniusPracticeDataCustomC
 import com.wotin.geniustest.customClass.geniusTest.GeniusTestDataCustomClass
 import com.wotin.geniustest.customClass.RetrofitGetGeniusPracticeAndTestDataCustomClass
 import com.wotin.geniustest.customClass.SignInAndSignUpCustomClass
+import com.wotin.geniustest.databinding.ActivityLoginBinding
 import com.wotin.geniustest.roomMethod.InsertRoomMethod
 import com.wotin.geniustest.roomMethod.UserRoomMethod
 import kotlinx.android.synthetic.main.activity_login.*
@@ -29,130 +31,136 @@ import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var mBinding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        mBinding.activity = this
 
-        login_button.setOnClickListener {
-            val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if(networkState(connectivityManager)) {
-
-                if (login_id_edittext.text!!.isNotEmpty() && login_password_edittext.text!!.isNotEmpty() && login_id_edittext!!.text!!.trim().length >= 7 && login_password_edittext!!.text!!.length >= 8) {
-                    signInAndSignUpApiService.signIn(
-                        id = EncryptionAndDetoxification()
-                            .encryptionAndDetoxification(login_id_edittext.text.toString()),
-                        password = EncryptionAndDetoxification()
-                            .encryptionAndDetoxification(login_password_edittext.text.toString())
-                    ).enqueue(object : Callback<SignInAndSignUpCustomClass> {
-                        override fun onFailure(call: Call<SignInAndSignUpCustomClass>, t: Throwable) {
-                            Log.d("TAG", "error... $t")
-                            Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_LONG).show()
-                        }
-
-                        override fun onResponse(
-                            call: Call<SignInAndSignUpCustomClass>,
-                            response: Response<SignInAndSignUpCustomClass>
-                        ) {
-                            try {
-                                if (response.body()!!.UniqueId == "") {
-                                    Toast.makeText(
-                                        applicationContext,
-                                        "존재하지 않는 사용자입니다.",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                } else {
-                                    Log.d(
-                                        "TAG",
-                                        "response.body data is ${response.body()!!.id} ${response.body()!!.password} ${response.body()!!.UniqueId} ${response.body()!!.name}"
-                                    )
-                                    Log.d(
-                                        "TAG", "response.body data is " +
-                                                EncryptionAndDetoxification()
-                                                    .encryptionAndDetoxification(response.body()!!.id) + " " +
-                                                EncryptionAndDetoxification()
-                                                    .encryptionAndDetoxification(response.body()!!.password) + " " +
-                                                response.body()!!.UniqueId + " " + response.body()!!.name
-                                    )
-                                    UserRoomMethod().insertUserData(
-                                        name = response.body()!!.name,
-                                        id = response.body()!!.id,
-                                        password = response.body()!!.password,
-                                        UniqueId = response.body()!!.UniqueId,
-                                        context = applicationContext
-                                    )
-                                    saveUserDataSharedPreference(response.body()!!.UniqueId, response.body()!!.id, response.body()!!.password)
-                                    getUserDataAndGeniusDataApiService.getGeniusData(response.body()!!.UniqueId).enqueue(object : Callback<RetrofitGetGeniusPracticeAndTestDataCustomClass> {
-                                        override fun onFailure(call: Call<RetrofitGetGeniusPracticeAndTestDataCustomClass>, t: Throwable) {
-                                            Toast.makeText(applicationContext, "데이터를 가져오는데 실패하였습니다", Toast.LENGTH_LONG).show()
-                                            Log.d("TAG", "getGeniusDataApiService error is $t")
-                                        }
-
-                                        override fun onResponse(
-                                            call: Call<RetrofitGetGeniusPracticeAndTestDataCustomClass>,
-                                            response: Response<RetrofitGetGeniusPracticeAndTestDataCustomClass>
-                                        ) {
-                                            val uniqueId = response.body()!!.UniqueId
-                                            val testLevel = response.body()!!.level
-                                            val bestScore = MapJsonConverter().MapToJsonConverter(response.body()!!.best_score.toString())
-                                            val practiceJson = MapJsonConverter().MapToJsonConverter(bestScore["practice"].toString())
-                                            val testJson = MapJsonConverter().MapToJsonConverter(bestScore["test"].toString())
-
-
-
-                                            val practice : GeniusPracticeDataCustomClass = GeniusPracticeDataCustomClass(UniqueId = uniqueId,
-                                                concentractionScore = practiceJson["practice_concentraction_score"].toString().toFloat().toInt().toString(),
-                                                memoryScore = practiceJson["practice_memory_score"].toString().toFloat().toInt().toString(),
-                                                concentractionDifference = practiceJson["practice_concentraction_difference"].toString(),
-                                                memoryDifference = practiceJson["practice_memory_difference"].toString(),
-                                                quicknessScore = practiceJson["practice_quickness_score"].toString(),
-                                                quicknessDifference = practiceJson["practice_quickness_difference"].toString())
-
-                                            val test : GeniusTestDataCustomClass = GeniusTestDataCustomClass(UniqueId = uniqueId,
-                                                concentractionScore = testJson["test_concentraction_score"].toString().toFloat().toInt().toString(),
-                                                memoryScore = testJson["test_memory_score"].toString().toFloat().toInt().toString(),
-                                                concentractionDifference = testJson["test_concentraction_difference"].toString(),
-                                                memoryDifference = testJson["test_memory_difference"].toString(),
-                                                quicknessScore = testJson["test_quickness_score"].toString(),
-                                                quicknessDifference = testJson["test_quickness_difference"].toString(),
-                                                level = testLevel)
-                                            Log.d("TAG",
-                                                "getGeniusDataApiService bestScore is $bestScore")
-                                            Log.d(
-                                                "TAG",
-                                                "getGeniusDataApiService practice is $practice, test is $test"
-                                            )
-                                            InsertRoomMethod().insertGeniusPracticeData(practice, applicationContext)
-                                            InsertRoomMethod().insertGeniusTestData(test, applicationContext)
-                                            InsertRoomMethod().insertTestModeData(applicationContext)
-                                            val intent =
-                                                Intent(this@LoginActivity, MainActivity::class.java)
-                                            startActivity(intent)
-                                            finish()
-                                        }
-                                    })
-                                }
-                            } catch (e: Exception) {
-                                Log.d("TAG", "error is ${e.message}")
-                                Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_LONG).show()
-                            }
-                        }
-
-                    })
-                } else if(login_id_edittext!!.text!!.isEmpty() || login_password_edittext.text!!.isEmpty()) Toast.makeText(applicationContext, "id 와 password 를 입력해주세요", Toast.LENGTH_LONG).show()
-                else if(login_id_edittext!!.text!!.trim().length < 7 || login_password_edittext.text!!.length < 8) Toast.makeText(applicationContext, "id 와 password 를 각각 7, 8 자 이상 입력해주세요", Toast.LENGTH_LONG).show()
-            } else Toast.makeText(applicationContext, "네트워크 연결상태가 좋지 못합니다.", Toast.LENGTH_LONG).show()
-        }
-
-        go_to_signup_activity_button.setOnClickListener {
+        mBinding.goToSignupActivityButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        forgot_id_textview.setOnClickListener {
-            showForgotIdDialog()
-        }
 
+    }
+
+    fun clickedLoginButton() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(networkState(connectivityManager)) {
+
+            if (mBinding.loginIdEdittext.text!!.isNotEmpty() && mBinding.loginPasswordEdittext.text!!.isNotEmpty() && mBinding.loginIdEdittext.text!!.trim().length >= 7 && mBinding.loginPasswordEdittext!!.text!!.length >= 8) {
+                signInAndSignUpApiService.signIn(
+                    id = EncryptionAndDetoxification()
+                        .encryptionAndDetoxification(mBinding.loginIdEdittext.text.toString()),
+                    password = EncryptionAndDetoxification()
+                        .encryptionAndDetoxification(mBinding.loginPasswordEdittext.text.toString())
+                ).enqueue(object : Callback<SignInAndSignUpCustomClass> {
+                    override fun onFailure(call: Call<SignInAndSignUpCustomClass>, t: Throwable) {
+                        Log.d("TAG", "error... $t")
+                        Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onResponse(
+                        call: Call<SignInAndSignUpCustomClass>,
+                        response: Response<SignInAndSignUpCustomClass>
+                    ) {
+                        try {
+                            if (response.body()!!.UniqueId == "") {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "존재하지 않는 사용자입니다.",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            } else {
+                                Log.d(
+                                    "TAG",
+                                    "response.body data is ${response.body()!!.id} ${response.body()!!.password} ${response.body()!!.UniqueId} ${response.body()!!.name}"
+                                )
+                                Log.d(
+                                    "TAG", "response.body data is " +
+                                            EncryptionAndDetoxification()
+                                                .encryptionAndDetoxification(response.body()!!.id) + " " +
+                                            EncryptionAndDetoxification()
+                                                .encryptionAndDetoxification(response.body()!!.password) + " " +
+                                            response.body()!!.UniqueId + " " + response.body()!!.name
+                                )
+                                UserRoomMethod().insertUserData(
+                                    name = response.body()!!.name,
+                                    id = response.body()!!.id,
+                                    password = response.body()!!.password,
+                                    UniqueId = response.body()!!.UniqueId,
+                                    context = applicationContext
+                                )
+                                saveUserDataSharedPreference(response.body()!!.UniqueId, response.body()!!.id, response.body()!!.password)
+                                getUserDataAndGeniusDataApiService.getGeniusData(response.body()!!.UniqueId).enqueue(object : Callback<RetrofitGetGeniusPracticeAndTestDataCustomClass> {
+                                    override fun onFailure(call: Call<RetrofitGetGeniusPracticeAndTestDataCustomClass>, t: Throwable) {
+                                        Toast.makeText(applicationContext, "데이터를 가져오는데 실패하였습니다", Toast.LENGTH_LONG).show()
+                                        Log.d("TAG", "getGeniusDataApiService error is $t")
+                                    }
+
+                                    override fun onResponse(
+                                        call: Call<RetrofitGetGeniusPracticeAndTestDataCustomClass>,
+                                        response: Response<RetrofitGetGeniusPracticeAndTestDataCustomClass>
+                                    ) {
+                                        val uniqueId = response.body()!!.UniqueId
+                                        val testLevel = response.body()!!.level
+                                        val bestScore = MapJsonConverter().MapToJsonConverter(response.body()!!.best_score.toString())
+                                        val practiceJson = MapJsonConverter().MapToJsonConverter(bestScore["practice"].toString())
+                                        val testJson = MapJsonConverter().MapToJsonConverter(bestScore["test"].toString())
+
+
+
+                                        val practice : GeniusPracticeDataCustomClass = GeniusPracticeDataCustomClass(UniqueId = uniqueId,
+                                            concentractionScore = practiceJson["practice_concentraction_score"].toString().toFloat().toInt().toString(),
+                                            memoryScore = practiceJson["practice_memory_score"].toString().toFloat().toInt().toString(),
+                                            concentractionDifference = practiceJson["practice_concentraction_difference"].toString(),
+                                            memoryDifference = practiceJson["practice_memory_difference"].toString(),
+                                            quicknessScore = practiceJson["practice_quickness_score"].toString(),
+                                            quicknessDifference = practiceJson["practice_quickness_difference"].toString())
+
+                                        val test : GeniusTestDataCustomClass = GeniusTestDataCustomClass(UniqueId = uniqueId,
+                                            concentractionScore = testJson["test_concentraction_score"].toString().toFloat().toInt().toString(),
+                                            memoryScore = testJson["test_memory_score"].toString().toFloat().toInt().toString(),
+                                            concentractionDifference = testJson["test_concentraction_difference"].toString(),
+                                            memoryDifference = testJson["test_memory_difference"].toString(),
+                                            quicknessScore = testJson["test_quickness_score"].toString(),
+                                            quicknessDifference = testJson["test_quickness_difference"].toString(),
+                                            level = testLevel)
+                                        Log.d("TAG",
+                                            "getGeniusDataApiService bestScore is $bestScore")
+                                        Log.d(
+                                            "TAG",
+                                            "getGeniusDataApiService practice is $practice, test is $test"
+                                        )
+                                        InsertRoomMethod().insertGeniusPracticeData(practice, applicationContext)
+                                        InsertRoomMethod().insertGeniusTestData(test, applicationContext)
+                                        InsertRoomMethod().insertTestModeData(applicationContext)
+                                        val intent =
+                                            Intent(this@LoginActivity, MainActivity::class.java)
+                                        startActivity(intent)
+                                        finish()
+                                    }
+                                })
+                            }
+                        } catch (e: Exception) {
+                            Log.d("TAG", "error is ${e.message}")
+                            Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                })
+            } else if(mBinding.loginIdEdittext.text!!.isEmpty() || mBinding.loginPasswordEdittext.text!!.isEmpty())
+                Toast.makeText(applicationContext, "id 와 password 를 입력해주세요", Toast.LENGTH_LONG).show()
+            else if(mBinding.loginIdEdittext.text!!.trim().length < 7 || mBinding.loginPasswordEdittext.text!!.length < 8)
+                Toast.makeText(applicationContext, "id 와 password 를 각각 7, 8 자 이상 입력해주세요", Toast.LENGTH_LONG).show()
+        } else Toast.makeText(applicationContext, "네트워크 연결상태가 좋지 못합니다.", Toast.LENGTH_LONG).show()
+    }
+
+    fun clickedFindIdButton() {
+        showForgotIdDialog()
     }
 
     private fun saveUserDataSharedPreference(UID : String, id : String, password : String) {
